8ec6cef7a8c77fdd903f8497e36636d83082bc61
UsmanHaiderAli <theUsmanhHaider@gmail.com>
2022-08-02 18:43:56 +0500

    WhatsApp UI App completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WhatsApp UI',
+      title: 'WhatsApp UI App',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
